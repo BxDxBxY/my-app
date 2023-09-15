7d510f68f9a99b1b0a0d720d823fd055e312e6d4
Jack <sunatila6391@email.com>
2023-09-15 06:00:22 -1200

    commit changes

---
+++ pages/jobs.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+const Jobs = () => {
+  return (
+    <>
+<div class="w-full  bg-gradient-to-br  via-[#0077b6] to-[#03045e]  from-[#caf0f8]  h-screen">
+hellow world
+</div>
+    </>
+  );
+};
+
+export default Jobs;
