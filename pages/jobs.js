64bfc0792e920d2259157a8c2bf8047845fbf77b
Jack <sunatila6391@email.com>
2023-09-15 06:03:07 -1200

    commit changes

--- pages/jobs.js
+++ pages/jobs.js
@@ -3,9 +3,9 @@ import { React } from "react";
 const Jobs = () => {
   return (
     <>
-<div class="w-full  bg-gradient-to-br  via-[#0077b6] to-[#03045e]  from-[#caf0f8]  h-screen">
-hellow world
-</div>
+      <div class="w-full  bg-gradient-to-br  via-[#0077b6] to-[#03045e]  from-[#caf0f8]  h-screen">
+        hellow world
+      </div>
     </>
   );
 };
